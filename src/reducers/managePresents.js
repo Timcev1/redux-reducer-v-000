e96ab93764ec65c28f6b5e56eee5f79170f27cd9
Timothy Cevallos <timcev@gmail.com>
2017-10-16 02:20:48 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,6 +2,7 @@ export function managePresents(state, action){
   switch (action.type) {
     case "INCREASE":
       return { numberOfPresents: state.numberOfPresents +1};
+      break;
     default:
       return state,
   }
