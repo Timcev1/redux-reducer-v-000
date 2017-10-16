43307eecb49a72ab1afa0e009f53535f55b22704
Timothy Cevallos <timcev@gmail.com>
2017-10-16 01:58:18 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+  friends: [],
+}, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return Object.assign({}, state {
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      });
+      
+      break;
+    default:
+      
+  }
 }
